c449186663daf3728dcd0d735171eaa266a8b191
LKlemens <kiti1990@tuta.io>
2020-07-05 21:28:59 +0200

    setup channel on phoenix side

--- assets/js/app.js
+++ assets/js/app.js
@@ -9,7 +9,7 @@ import "../css/app.scss";
 //
 // Import deps with the dep name or local files with a relative path, for example:
 //
-//     import {Socket} from "phoenix"
+import { Socket } from "phoenix";
 //     import socket from "./socket"
 //
 import "phoenix_html";
@@ -17,6 +17,34 @@ import "phoenix_html";
 // Elm
 
 import { Elm } from "../elm/src/Main.elm";
-Elm.Main.init({
-  node: document.getElementById("elm-container"),
+
+let socketParams = window.userToken == "" ? {} : { token: window.userToken };
+let socket = new Socket("/socket", {
+  params: socketParams,
 });
+
+socket.connect();
+
+const elmContainer = document.querySelector("#elm-container");
+
+if (elmContainer) {
+  let app = Elm.Main.init({ node: elmContainer });
+  let channel = socket.channel("room:chat", {});
+
+  channel
+    .join()
+    .receive("ok", (resp) => {
+      console.log("Joined successfully", resp);
+    })
+    .receive("error", (resp) => {
+      console.log("Unable to join", resp);
+    });
+
+  app.ports.sendMessage.subscribe(function (name) {
+    console.log(
+      `Broadcasting ${name} score data from Elm using the sendMessage port.`
+    );
+    channel.push("broadcast_custom", { name: name });
+    // Later, we'll push the score data to the Phoenix channel
+  });
+}
