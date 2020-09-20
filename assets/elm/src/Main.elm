a4c72f7f3508fd072a8f4feb27ff1206f46ef47b
LKlemens <kiti1990@tuta.io>
2020-09-20 19:04:50 +0200

    change Browser.element to Browser.application

--- assets/elm/src/Main.elm
+++ assets/elm/src/Main.elm
@@ -1,11 +1,14 @@
 port module Main exposing (main)
 
 import Browser
-import Html exposing (Html, button, div, input, text)
-import Html.Attributes exposing (placeholder)
-import Html.Events
+import Browser.Navigation as Nav
+import Html exposing (Html, button, div, form, input, li, text, ul)
+import Html.Attributes exposing (placeholder, type_, value)
+import Html.Events as Events
 import Json.Decode as Decode
 import Json.Encode as Encode
+import Url
+import Url.Parser exposing ((</>), Parser, int, map, oneOf, s, string)
 
 
 
@@ -13,12 +16,15 @@ import Json.Encode as Encode
 -- main : Html
 
 
+main : Program () Model Msg
 main =
-    Browser.element
+    Browser.application
         { init = init
         , view = view
-        , subscriptions = subscriptions
         , update = update
+        , subscriptions = subscriptions
+        , onUrlChange = UrlChanged
+        , onUrlRequest = LinkClicked
         }
 
 
@@ -36,8 +42,22 @@ port messageReceiver : (Encode.Value -> msg) -> Sub msg
 -- MODEL
 
 
+type Route
+    = Name String
+
+
+routeParser : Parser (String -> a) a
+routeParser =
+    s "" </> string
+
+
 type alias Model =
     { name : String
+    , username : String
+    , msg : String
+    , msgs : List String
+    , key : Nav.Key
+    , url : Url.Url
     }
 
 
@@ -46,15 +66,20 @@ initialCommand =
     Cmd.none
 
 
-initialModel : Model
-initialModel =
-    { name = "popo"
+initialModel : Url.Url -> Nav.Key -> Model
+initialModel url key =
+    { name = ""
+    , username = ""
+    , msg = ""
+    , msgs = []
+    , key = key
+    , url = url
     }
 
 
-init : () -> ( Model, Cmd Msg )
-init _ =
-    ( initialModel, initialCommand )
+init : () -> Url.Url -> Nav.Key -> ( Model, Cmd Msg )
+init flags url key =
+    ( initialModel url key, initialCommand )
 
 
 
@@ -65,6 +90,10 @@ type Msg
     = Init
     | BroadcastCustom String
     | ReceiveMsg Encode.Value
+    | HandleLogin String
+    | HandleMsg String
+    | LinkClicked Browser.UrlRequest
+    | UrlChanged Url.Url
 
 
 stringDecoder =
@@ -75,10 +104,20 @@ update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
     case msg of
         Init ->
-            ( initialModel, Cmd.none )
+            let
+                name =
+                    routeParser
+            in
+            ( model, Cmd.none )
 
-        BroadcastCustom name ->
-            ( { model | name = name }, sendMessage name )
+        BroadcastCustom message ->
+            ( { model | msgs = model.msgs ++ [ message ] }, sendMessage message )
+
+        HandleMsg message ->
+            ( { model | msg = message }, Cmd.none )
+
+        HandleLogin username ->
+            ( { model | username = username }, Cmd.none )
 
         ReceiveMsg jsonName ->
             case Decode.decodeValue stringDecoder jsonName of
@@ -88,6 +127,17 @@ update msg model =
                 Err message ->
                     Debug.log ("Error receiving name " ++ Debug.toString message)
                         ( model, Cmd.none )
+
+        LinkClicked urlRequest ->
+            case urlRequest of
+                Browser.Internal url ->
+                    ( model, Nav.pushUrl model.key (Url.toString url) )
+
+                Browser.External href ->
+                    ( model, Nav.load href )
+
+        UrlChanged url ->
+            ( { model | url = url }, Cmd.none )
 
 
 
@@ -103,24 +153,42 @@ subscriptions model =
 -- VIEW
 
 
-view : Model -> Html Msg
+view : Model -> Browser.Document Msg
 view model =
-    div []
-        [ text model.name
-        , viewBroadcastButton model
+    { title = "url"
+    , body =
+        [ div []
+            [ form []
+                [ viewMsgs model.msgs
+                , viewInput "input" "write msg" model.msg HandleMsg
+                , sendButton model
+                ]
+            ]
         ]
+    }
 
 
-viewBroadcastButton : Model -> Html Msg
-viewBroadcastButton model =
+viewMsgs : List String -> Html msg
+viewMsgs list =
+    ul [] (List.map (\m -> li [] [ text m ]) list)
+
+
+viewInput : String -> String -> String -> (String -> msg) -> Html msg
+viewInput t p v toMsg =
+    input [ type_ t, placeholder p, value v, Events.onInput toMsg ] []
+
+
+sendButton : Model -> Html Msg
+sendButton model =
     let
         broadcastEvent =
-            model.name
+            model.msg
                 |> BroadcastCustom
-                |> Html.Events.onClick
+                |> Events.onClick
     in
     button
         [ broadcastEvent
-        , Html.Attributes.class "button"
+        , type_ "button"
+        , Html.Attributes.class "button is-danger"
         ]
-        [ text "Broadcast Over Socket" ]
+        [ text "send" ]
