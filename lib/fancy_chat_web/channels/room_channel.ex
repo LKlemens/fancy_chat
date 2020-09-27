d59afd1d9c7776de4534cd8a0c78c927118931d9
LKlemens <klemens.lukaszczyk@gmail.com>
2020-09-27 08:22:01 +0200

    basic chat functionality - users can choose friend for conversation

--- lib/fancy_chat_web/channels/room_channel.ex
+++ lib/fancy_chat_web/channels/room_channel.ex
@@ -8,29 +8,28 @@ defmodule FancyChatWeb.RoomChannel do
     socket = assign(socket, :name, payload["name"])
     IO.inspect({:jooooooooooooin2, socket})
     IO.inspect({:plaaaaaaaaaaaaaaa, payload})
-    # {:ok, Map.put(socket.assigns, :name, payload["name"])}
     {:ok, socket}
   end
 
   def handle_in("come_online", payload, socket) do
     IO.inspect({:come_online, payload})
-    # broadcast(socket, "online_users", payload)
 
     FancyChatWeb.Endpoint.broadcast!("room:general", "online_users", payload)
     push(socket, "online_users", payload)
     {:noreply, socket}
   end
 
-  def handle_in("broadcast_custom", payload, socket) do
+  def handle_in("send_message", payload, socket) do
     IO.puts("hello gus")
     IO.inspect(payload)
     IO.inspect({:jooooooooooooin3, socket})
-    # [{p, _}] = Registry.lookup(Registry.Users, )
-    broadcast(socket, "broadcast_custom", Map.put(payload, :name, socket.assigns.name))
+    id_receiver = GenServer.call(FancyChat.AllUsers, {:get_id, payload["message"]["receiver"]})
+    id_sender = GenServer.call(FancyChat.AllUsers, {:get_id, payload["message"]["sender"]})
+    message = "#{payload["message"]["sender"]}: #{payload["message"]["msg"]}"
 
-    # broadcast(socket, "online_users", payload)
-    # FancyChat.Users.push(payload.name)
-    # FancyChatWeb.Endpoint.broadcast!("room:#{id}", "msg", %{})
+    FancyChatWeb.Endpoint.broadcast!("room:#{id_receiver}", "send_message", %{msg: message})
+
+    FancyChatWeb.Endpoint.broadcast!("room:#{id_sender}", "send_message", %{msg: message})
 
     {:noreply, socket}
   end
